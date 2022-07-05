57d52bf793de74091b9406ecaae9d1fefb0bb921
Taz <47963508+Web-Dev-Taz@users.noreply.github.com>
2022-07-05 12:55:17 -0600

    added delete tweet

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import { useSession } from "next-auth/react"
 import { useRouter } from "next/router"
+import Tweets from "components/Tweets"
+import prisma from "lib/prisma"
+import { getTweets } from "lib/data.js"
 
-export default function Home() {
+export default function Index({ tweets }) {
    const { data: session, status } = useSession()
    const router = useRouter()
 
@@ -13,5 +16,32 @@ export default function Home() {
       router.push("/home")
    }
 
-   return <a href="/api/auth/signin">login</a>
+   return (
+      <div className="mt-10">
+         <Tweets tweets={tweets} />
+         <div className="text-center p-4 border m-4">
+            <h2 className="mb-10">Join the conversation!</h2>
+            <a
+               className="border px-8 py-2 mt-5 font-bold rounded-full color-accent-contrast bg-color-accent hover:bg-color-accent-hover-darker"
+               href="/api/auth/signin"
+            >
+               login
+            </a>
+         </div>
+      </div>
+   )
 }
+
+export async function getServerSideProps() {
+   const take = 3
+   let tweets = await getTweets(prisma, take)
+   tweets = JSON.parse(JSON.stringify(tweets))
+
+   return {
+      props: {
+         tweets,
+      },
+   }
+}
+
+// Home Page for Logged Out Users
